edca29c8fed805c052ed40a4e8a1f2dc5e0961de
PAPPURU MANJUNATHA <you@example.com>
2026-03-10 00:11:48 +0530

    Convert to ES6 modules

--- api/create-order.js
+++ api/create-order.js
@@ -1,6 +1,6 @@
-const axios = require('axios');
+import axios from 'axios';
 
-module.exports = async function handler(req, res) {
+export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
@@ -83,4 +83,4 @@ module.exports = async function handler(req, res) {
       details: error.response?.data
     });
   }
-};
+}
